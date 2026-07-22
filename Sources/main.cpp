ba531bb9a7e7bd62bfb69934a19134c766166a86
Motisma479 <robion.mathieu.16@gmail.com>
2026-07-22 11:43:23 +0200

    [Feature]Interface done

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,4 +1,14 @@
 #include "pch.hpp"
+#include <glad/gl.h>
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
+#include <imgui.h>
+#include <backends/imgui_impl_glfw.h>
+#include <backends/imgui_impl_opengl3.h>
+#include <misc/cpp/imgui_stdlib.h>
+
+#include <cmath>
 
 #if defined(_MSC_VER) && defined(_DEBUG)
 #include <crtdbg.h>
@@ -14,5 +24,90 @@ int main(int argc, char** argv)
 
 	std::cout << "Hello World!\n";
 
-	return 0;
+	GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+	if (!gladLoadGL(glfwGetProcAddress))
+	{
+		glfwDestroyWindow(window);
+		glfwTerminate();
+		return -1;
+	}
+
+	ImGui::CreateContext();
+	ImGuiIO* guiIO = &ImGui::GetIO();
+	guiIO->ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+	guiIO->ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+
+	ImGui::StyleColorsDark();
+
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init("#version 460");
+
+	std::string username;
+	bool useMCImage = true;
+	std::string message;
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwPollEvents();
+
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		ImGuiWindowFlags flags = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_MenuBar;
+		const ImGuiViewport* viewport = ImGui::GetMainViewport();
+		ImGui::SetNextWindowPos(viewport->Pos);
+		ImGui::SetNextWindowSize(viewport->Size);
+		ImGui::Begin("Main Window", NULL, flags);
+
+		if (ImGui::BeginMenuBar())
+		{
+			if (ImGui::BeginMenu("File"))
+			{
+				if (ImGui::MenuItem("Close", "Alt+f4"))  { glfwSetWindowShouldClose(window, true); }
+				ImGui::EndMenu();
+			}
+			ImGui::EndMenuBar();
+		}
+
+		ImGui::Text("Username:");
+		ImGui::SameLine();
+		ImGui::InputText("##UsernameBox", &username);
+		ImGui::Checkbox("Use minecraft image", &useMCImage);//TODO
+		ImGui::Text("Message:");
+		ImGui::InputTextMultiline("##MessageBox", &message);
+		if(ImGui::Button("Send"))
+		{
+			message.clear();
+		}
+		ImGui::End();
+
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        glfwSwapBuffers(window);
+    }
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+	glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
 }
