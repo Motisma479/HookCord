76ea9fac48d730fb81655c23843feedb8508ba0d
Motisma479 <robion.mathieu.16@gmail.com>
2026-07-22 14:25:05 +0200

    [Feature] Add Curl for http/https communication

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -10,6 +10,8 @@
 
 #include <cmath>
 
+#include <curl/curl.h>
+
 #if defined(_MSC_VER) && defined(_DEBUG)
 #include <crtdbg.h>
 #endif
@@ -24,6 +26,22 @@ int main(int argc, char** argv)
 
 	std::cout << "Hello World!\n";
 
+	curl_version_info_data* info = curl_version_info(CURLVERSION_NOW);
+    std::cout << "Protocoles supportes par votre curl : " << std::endl;
+    
+    for (int i = 0; info->protocols[i] != nullptr; ++i) {
+        std::cout << "- " << info->protocols[i] << std::endl;
+    }
+
+	CURL* curl;
+	CURLcode result;
+	result = curl_global_init(CURL_GLOBAL_ALL);
+	if(result != CURLE_OK)
+		return 1;
+	
+	struct curl_slist *headers = NULL;
+	headers = curl_slist_append(headers, "Content-Type: application/json; charset=utf-8");
+	
 	GLFWwindow* window;
 
     /* Initialize the library */
@@ -58,6 +76,7 @@ int main(int argc, char** argv)
 	ImGui_ImplGlfw_InitForOpenGL(window, true);
 	ImGui_ImplOpenGL3_Init("#version 460");
 
+	std::string webhookURL;
 	std::string username;
 	bool useMCImage = true;
 	std::string message;
@@ -88,6 +107,9 @@ int main(int argc, char** argv)
 			ImGui::EndMenuBar();
 		}
 
+		ImGui::Text("webhook URL:");
+		ImGui::SameLine();
+		ImGui::InputText("##WebhookURLBox", &webhookURL);
 		ImGui::Text("Username:");
 		ImGui::SameLine();
 		ImGui::InputText("##UsernameBox", &username);
@@ -96,6 +118,22 @@ int main(int argc, char** argv)
 		ImGui::InputTextMultiline("##MessageBox", &message);
 		if(ImGui::Button("Send"))
 		{
+			curl = curl_easy_init();
+			if(curl) {
+				
+				std::string json = "{\n   \"username\": \"" + username + "\",\n   \"avatar_url\": \"https://mc-api.io/render/face/" + username + "/java?size=1024\",\n   \"content\": \"" + message + "\",\n   \"allowed_mentions\": {\"parse\":[\"everyone\", \"roles\", \"users\"]}\n}";
+				curl_easy_setopt(curl, CURLOPT_DEFAULT_PROTOCOL, "http");
+				curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+				curl_easy_setopt(curl, CURLOPT_URL, webhookURL.c_str());
+				curl_easy_setopt(curl, CURLOPT_POSTFIELDS, json.c_str());
+			
+				result = curl_easy_perform(curl);
+			
+				if(result != CURLE_OK)
+					std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(result) << std::endl;
+			
+				curl_easy_cleanup(curl);
+			}
 			message.clear();
 		}
 		ImGui::End();
@@ -104,6 +142,7 @@ int main(int argc, char** argv)
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         glfwSwapBuffers(window);
     }
+	curl_global_cleanup();
 	ImGui_ImplOpenGL3_Shutdown();
 	ImGui_ImplGlfw_Shutdown();
 	ImGui::DestroyContext();
